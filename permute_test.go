b6e14dcd0a239196f5fa8e1be3b90832c8e10d8b
Steve McCoy <mccoyst@gmail.com>
2014-07-15 16:07:21 -0400

    Delete unused code

--- permute_test.go
+++ permute_test.go
@@ -9,18 +9,6 @@ type ints []int
 func (n ints) Len() int { return len(n) }
 func (n ints) Less(i, j int) bool { return n[i] < n[j] }
 func (n ints) Swap(i, j int) { n[i], n[j] = n[j], n[i] }
-func (n ints) Slice(i, j int) Interface { return n[i:j] }
-func Eq(n, m ints) bool {
-	if n.Len() != m.Len() {
-		return false
-	}
-	for i := range n {
-		if n[i] != m[i] {
-			return false
-		}
-	}
-	return true
-}
 
 func ExamplePermute() {
 	n := ints{ 1, 2, 3 }
